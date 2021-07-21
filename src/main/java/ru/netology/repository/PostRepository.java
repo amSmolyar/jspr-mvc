edf7051571494847eb172a6b7bf25af314359a63
Smolyar Tonya <am.smolyar94@gmail.com>
2021-07-21 15:29:41 +0300

    task2. add removed flag

--- src/main/java/ru/netology/repository/PostRepository.java
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -1,41 +1,53 @@
 package ru.netology.repository;
 
+import ru.netology.exception.NotFoundException;
 import ru.netology.model.Post;
 
 import java.util.*;
 
 // Stub
 public class PostRepository {
-  private Map<Long, Post> postMap;
-  private Long idCnt = (long) 1;
+    private Map<Long, Post> postMap;
+    private Long idCnt = (long) 1;
 
-  public PostRepository() {
-    postMap = new HashMap<>();
-  }
-
-  public List<Post> all() {
-    List<Post> list = new ArrayList<>(postMap.values());
-    return list;
-  }
-
-  public Optional<Post> getById(long id) {
-    return (postMap.containsKey(id)) ? Optional.of(postMap.get(id)) : Optional.empty();
-  }
-
-  public synchronized Post save(Post post) {
-    if ((post.getId() == 0) || (!postMap.containsKey(post.getId()))) {
-      Post newPost = new Post(idCnt, post.getContent());
-      postMap.put(idCnt, newPost);
-      idCnt++;
-      return newPost;
+    public PostRepository() {
+        postMap = new HashMap<>();
     }
 
-    postMap.put(post.getId(), post);
-    return post;
-  }
+    public List<Post> all() {
+        List<Post> list = new ArrayList<>();
+        for (Post post : postMap.values()) {
+            if (!post.isRemoved())
+                list.add(post);
+        }
+        return list;
+    }
 
-  public synchronized void removeById(long id) {
-    if (postMap.containsKey(id))
-      postMap.remove(id);
-  }
+    public Optional<Post> getById(long id) {
+        return (postMap.containsKey(id) && (!postMap.get(id).isRemoved())) ? Optional.of(postMap.get(id)) : Optional.empty();
+    }
+
+    public synchronized Optional<Post> save(Post post) {
+        if ((post.getId() == 0) || (!postMap.containsKey(post.getId()))) {
+            Post newPost = new Post(idCnt, post.getContent());
+            postMap.put(idCnt, newPost);
+            idCnt++;
+            return Optional.of(newPost);
+        }
+
+        if (!postMap.get(post.getId()).isRemoved()) {
+            postMap.put(post.getId(), post);
+            return Optional.of(post);
+        } else
+            return Optional.empty();
+    }
+
+    public synchronized void removeById(long id) throws NotFoundException {
+        if (postMap.containsKey(id)) {
+            if (!postMap.get(id).isRemoved())
+                postMap.get(id).setRemoved(true);
+            else
+                throw new NotFoundException();
+        }
+    }
 }
--- src/main/java/ru/netology/service/PostService.java
+++ src/main/java/ru/netology/service/PostService.java
@@ -1,5 +1,7 @@
 package ru.netology.service;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import ru.netology.exception.NotFoundException;
 import ru.netology.model.Post;
 import ru.netology.repository.PostRepository;
@@ -7,26 +9,26 @@ import ru.netology.repository.PostRepository;
 import java.util.List;
 
 public class PostService {
-  private final PostRepository repository;
+    private final PostRepository repository;
 
-  public PostService(PostRepository repository) {
-    this.repository = repository;
-  }
+    public PostService(PostRepository repository) {
+        this.repository = repository;
+    }
 
-  public List<Post> all() {
-    return repository.all();
-  }
+    public List<Post> all() {
+        return repository.all();
+    }
 
-  public Post getById(long id) {
-    return repository.getById(id).orElseThrow(NotFoundException::new);
-  }
+    public Post getById(long id) {
+        return repository.getById(id).orElseThrow(NotFoundException::new);
+    }
 
-  public Post save(Post post) {
-    return repository.save(post);
-  }
+    public Post save(Post post) {
+        return repository.save(post).orElseThrow(NotFoundException::new);
+    }
 
-  public void removeById(long id) {
-    repository.removeById(id);
-  }
+    public void removeById(long id) {
+        repository.removeById(id);
+    }
 }
 
